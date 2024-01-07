6c739ab65fc89632e70476fde41a942081af5214
Ansh_13 <104825546+anshmendpara-13@users.noreply.github.com>
2024-01-07 08:30:23 +0530

    last

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   plugins: [react()],
 })
